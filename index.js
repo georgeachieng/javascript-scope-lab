96b8c1d82a6dea4dc38e46f6cdec699626d02a91
George Achieng <george.achieng@student.moringaschool.com>
2026-01-06 23:21:41 +0300

    revised and corrected the logic

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-// Write your solution in this file!
-const burgers = ["Hamburger","Cheeseburger"];
-    function addBurger() {
-let newBurger = "Flatburger";
-       burgers.push(newBurger);
-        return newBurger;
-    }
-addBurger(); // Adds  "Flatburger"
-let anotherNewBurger = "Maple Bacon Burger";
-     if (anotherNewBurger === "Maple Bacon Burger"){
-      console.log ("yummy");}
-     else  {console.log ("not yummy")}
-     burgers.push(anotherNewBurger);
-     console.log(burgers);
-     // ["Hamburger", "Cheeseburger", "Flatburger", "Maple Bacon Burger"]
+var burgers = ["Hamburger", "Cheeseburger"];
+var featuredDrink = "Strawberry Milkshake";
 
-const featuredDrinks = ["Strawberry","Milkshake"];
-   function changeFeaturedDrink(Drinks) {
-           Drinks[1] = "The Java Shake"; //Replace second item
-         return Drinks;
-   }
-changeFeaturedDrink(featuredDrinks);
 
-console.log(featuredDrinks);
+function addBurger() {
+  let newBurger = "Flatburger"; 
+  burgers.push(newBurger);
+}
+
+
+addBurger();
+
+
+if (true) {
+  const anotherNewBurger = "Maple Bacon Burger"; 
+  burgers.push(anotherNewBurger);
+
+  function changeFeaturedDrink() {
+    featuredDrink = "The JavaShake"; 
+  }
+
+
+  changeFeaturedDrink();
+}
+
+console.log("Burgers:", burgers);
+
+console.log("Featured Drink:", featuredDrink);
